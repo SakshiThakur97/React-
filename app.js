5450e9691c252790f6e494017b0b8bb085d2c398
sakshi thakur <thakur.17may2000sakshi@gmail.com>
2024-04-08 22:23:40 +0530

    chapter 1 in react

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
-const heading = React.createElement("h1",{},"hello react");
-   const root = ReactDOM.createRoot(document.getElementById("root"));
-   root.render(heading);
+
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
+//APP LAYOUT{
+// Heading - Logo , navigation bar
+
+//body - food cart , search bar 
+//footer- links , about section }
+
+
+
+// app layout - this contains the whole layout.
+
+const Heading = () => {
+    return( <div className="header">
+             <div>
+              <img className="logo" src="https://cdn.vectorstock.com/i/500p/60/58/chef-cook-food-logo-vector-14736058.jpg" /> 
+             </div> 
+
+             <div className="navigation"> 
+               <ul>
+                  <li>Home</li>  
+                 <li>About Us</li>
+                 <li>Contact us </li>
+                 <li> Cart</li>
+               </ul>
+
+            
+            </div>
+
+             
+         </div>
+);
+};
+
+
+
+
+const AppLayout = () =>{
+  return <div className="app">
+    <Heading/>
+  </div> 
+
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")) 
+root.render(<AppLayout/>)
